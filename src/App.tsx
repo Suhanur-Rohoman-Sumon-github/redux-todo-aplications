ad10992a7eb070a8386c5243f2ebe85687cf1874
suhanur-rohoman-sumon <sumonsuhanurrohoman@gmail.com>
2024-01-21 18:55:13 +0600

    redux setup done

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import { Button } from "./components/ui/button";
+
+const App = () => {
+  return (
+    <div>
+      <h1>home pge</h1>
+      <Button>this is button</Button>
+    </div>
+  );
+};
+
+export default App;
